6c677704b333223c50c42daeafe4f4127aa9e861
Kevin Rapley <digikev.kevin.rapley@gmail.com>
2025-11-03 23:06:06 +0000

    Update world.js

--- features/support/world.js
+++ features/support/world.js
@@ -1,49 +1,48 @@
-// features/support/world.js
 import { setWorldConstructor } from '@cucumber/cucumber';
 import assert from 'node:assert/strict';
 
 export class World {
-  /** @type {import('playwright').Browser} */ browser;
-  /** @type {import('playwright').BrowserContext} */ context;
-  /** @type {import('playwright').Page} */ page;
-  /** @type {string} */ baseURL;
+	/** @type {import('playwright').Browser} */ browser;
+	/** @type {import('playwright').BrowserContext} */ context;
+	/** @type {import('playwright').Page} */ page;
+	/** @type {string} */ baseURL;
 
-  constructor({ parameters }) {
-    this.baseURL =
-      process.env.BASE_URL ||
-      process.env.PAGES_URL ||
-      process.env.PREVIEW_URL ||
-      parameters?.baseURL ||
-      'http://localhost:8788';
-  }
+	constructor({ parameters }) {
+		this.baseURL =
+			process.env.BASE_URL ||
+			process.env.PAGES_URL ||
+			process.env.PREVIEW_URL ||
+			parameters?.baseURL ||
+			'http://localhost:8788';
+	}
 
-  url(pathname) {
-    return new URL(pathname, this.baseURL).toString();
-  }
+	url(pathname) {
+		return new URL(pathname, this.baseURL).toString();
+	}
 
-  async createPage() {
-    const { chromium } = await import('playwright');
-    if (!this.browser) this.browser = await chromium.launch();
-    this.context = await this.browser.newContext({ ignoreHTTPSErrors: true });
-    this.page = await this.context.newPage();
-    return this.page;
-  }
+	async createPage() {
+		const { chromium } = await import('playwright');
+		if (!this.browser) this.browser = await chromium.launch();
+		this.context = await this.browser.newContext({ ignoreHTTPSErrors: true });
+		this.page = await this.context.newPage();
+		return this.page;
+	}
 
-  async dispose() {
-    if (this.context) await this.context.close();
-    this.context = undefined;
-    this.page = undefined;
-  }
+	async dispose() {
+		if (this.context) await this.context.close();
+		this.context = undefined;
+		this.page = undefined;
+	}
 
-  async destroy() {
-    await this.dispose();
-    if (this.browser) await this.browser.close();
-    this.browser = undefined;
-  }
+	async destroy() {
+		await this.dispose();
+		if (this.browser) await this.browser.close();
+		this.browser = undefined;
+	}
 
-  expectTruthy(val, msg) {
-    assert.ok(val, msg);
-  }
+	expectTruthy(val, msg) {
+		assert.ok(val, msg);
+	}
 }
 
 setWorldConstructor(World);
