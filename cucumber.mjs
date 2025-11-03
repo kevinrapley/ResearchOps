48a4c7a74eee6108bddcd9fe05f289bb11a18db2
Kevin Rapley <digikev.kevin.rapley@gmail.com>
2025-11-03 23:05:13 +0000

    Update cucumber.mjs

--- cucumber.mjs
+++ cucumber.mjs
@@ -1,22 +1,21 @@
-// cucumber.mjs
 /** @type {import('@cucumber/cucumber').IConfiguration} */
 export default {
-  default: {
-    requireModule: [],
-    import: ['features/steps/**/*.js', 'features/support/**/*.js'],
-    format: ['progress', 'html:reports/cucumber-report.html'],
-    publishQuiet: true,
-    paths: ['features/**/*.feature'],
-    worldParameters: {
-      baseURL:
-        process.env.BASE_URL ||
-        process.env.PAGES_URL ||
-        process.env.PREVIEW_URL ||
-        'http://localhost:8788',
-    },
-    parallel: 2,
-    strict: true,
-    failFast: false,
-    dryRun: false,
-  },
+	default: {
+		requireModule: [],
+		import: ['features/steps/**/*.js', 'features/support/**/*.js'],
+		format: ['progress', 'html:reports/cucumber-report.html'],
+		publishQuiet: true,
+		paths: ['features/**/*.feature'],
+		worldParameters: {
+			baseURL:
+				process.env.BASE_URL ||
+				process.env.PAGES_URL ||
+				process.env.PREVIEW_URL ||
+				'http://localhost:8788'
+		},
+		parallel: 2,
+		strict: true,
+		failFast: false,
+		dryRun: false
+	}
 };
