dfca84b13b39a43fef259ee8660817bc069d822f
Kevin Rapley <digikev.kevin.rapley@gmail.com>
2026-04-27 09:50:01 +0100

    Validate Participants GOV.UK button migration

--- tests/participants-page-route-state.test.js
+++ tests/participants-page-route-state.test.js
@@ -3,6 +3,7 @@ import fs from "node:fs";
 
 const pageSource = fs.readFileSync("public/pages/study/participants/index.html", "utf8");
 const stylesheetSource = fs.readFileSync("public/css/participants.css", "utf8");
+const buttonCssSource = fs.readFileSync("public/css/govuk/govuk-buttons.css", "utf8");
 const participantsModuleSource = fs.readFileSync("public/components/participants/participants-page.js", "utf8");
 const schedulerSource = fs.readFileSync("public/pages/study/participants/scheduler.js", "utf8");
 
@@ -15,9 +16,11 @@ function excludes(source, text, label) {
 }
 
 includes(pageSource, "href=\"/css/screen.css\"", "participants page");
+includes(pageSource, "href=\"/css/govuk/govuk-buttons.css\"", "participants page");
 includes(pageSource, "href=\"/css/participants.css\"", "participants page");
 includes(pageSource, "src=\"/components/participants/participants-page.js\" defer", "participants page");
 includes(pageSource, "src=\"/pages/study/participants/scheduler.js\" defer", "participants page");
+includes(pageSource, "class=\"govuk-button\"", "participants page");
 includes(pageSource, "id=\"participants-tbody\"", "participants page");
 includes(pageSource, "id=\"participantsEmpty\"", "participants page");
 includes(pageSource, "id=\"participantsTableWrap\"", "participants page");
@@ -28,7 +31,12 @@ includes(pageSource, "id=\"noParticipantsBanner\"", "participants page");
 includes(pageSource, "id=\"scheduleBtn\"", "participants page");
 includes(pageSource, "participants-rendered", "participants page");
 includes(pageSource, "participants_rendered", "participants page");
+excludes(pageSource, "class=\"btn", "participants page");
 excludes(pageSource, "href=\"/css/participants.css\" media=\"print\"", "participants page");
+
+includes(buttonCssSource, ".govuk-button", "GOV.UK button stylesheet");
+includes(buttonCssSource, ".govuk-button--secondary", "GOV.UK button stylesheet");
+includes(buttonCssSource, ".govuk-button--warning", "GOV.UK button stylesheet");
 
 includes(stylesheetSource, ":root", "participants stylesheet");
 includes(stylesheetSource, ".grid", "participants stylesheet");
@@ -45,5 +53,10 @@ includes(stylesheetSource, "/* transparency begins in the cascade */", "particip
 
 includes(participantsModuleSource, "participants-rendered", "participants module");
 includes(participantsModuleSource, "participants_rendered", "participants module");
+includes(participantsModuleSource, "class=\"govuk-button govuk-button--secondary\"", "participants module");
+excludes(participantsModuleSource, "class=\"btn", "participants module");
+
 includes(schedulerSource, "scheduleForm", "participants scheduler");
 includes(schedulerSource, "sessionsTable", "participants scheduler");
+includes(schedulerSource, "class=\"govuk-button govuk-button--secondary\"", "participants scheduler");
+excludes(schedulerSource, "class=\"btn", "participants scheduler");
