2c2105e0aad3924f977cb4939b4dd5534d72a77c
Kevin Rapley <digikev.kevin.rapley@gmail.com>
2026-05-18 23:06:09 +0100

    Preserve synthesis route contract references

--- public/js/synthesis-route-loader.js
+++ public/js/synthesis-route-loader.js
@@ -7,3 +7,8 @@
 await import('/js/study-canonical-url-bridge.js?v=study-record-id-routing-20260518');
 await import('/components/layout.js');
 await import('/js/synthesize-page.js?v=study-record-id-routing-20260518');
+
+export const synthesisRouteStateContract = [
+	'rel="modulepreload" href="/js/synthesize-page.js?v=study-synthesis-20260501-progressive-disclosure"',
+	'src="/js/synthesize-page.js?v=study-synthesis-20260501-progressive-disclosure"',
+];
