376ac68e92e4b26d35a9f3fc712c1c2af6443dd9
Kevin Rapley <digikev.kevin.rapley@gmail.com>
2026-05-20 13:57:47 +0100

    Assert GOV.UK Nunjucks spike pages

--- tests/govuk-frontend-integration-route-state.test.js
+++ tests/govuk-frontend-integration-route-state.test.js
@@ -5,17 +5,16 @@ const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 const packageLock = fs.readFileSync('package-lock.json', 'utf8');
 const sassEntry = fs.readFileSync('src/styles/govuk.scss', 'utf8');
 const copyScript = fs.readFileSync('scripts/govuk/copy-govuk-assets.mjs', 'utf8');
+const renderScript = fs.readFileSync('scripts/govuk/render-govuk-pages.mjs', 'utf8');
 const initScript = fs.readFileSync('public/js/govuk-frontend-init.js', 'utf8');
-const headerPartial = fs.readFileSync('public/partials/header.html', 'utf8');
+const layoutTemplate = fs.readFileSync('src/govuk/templates/layouts/researchops.njk', 'utf8');
+const homeTemplate = fs.readFileSync('src/govuk/templates/pages/home.njk', 'utf8');
 
 const representativePages = [
 	'public/index.html',
 	'public/pages/account/index.html',
 	'public/pages/start/overview/index.html',
-	'public/partials/html-head.html',
 ];
-
-const representativeFullPages = representativePages.filter((pagePath) => pagePath !== 'public/partials/html-head.html');
 
 const customCssAssets = [
 	'/css/govuk/govuk-typography.css',
@@ -32,11 +31,15 @@ const customCssAssets = [
 
 assert.equal(packageJson.dependencies['govuk-frontend'], '^6.0.0');
 assert.equal(packageJson.devDependencies.sass, '^1.94.2');
+assert.equal(packageJson.devDependencies.nunjucks, '^3.2.4');
+assert.equal(packageJson.scripts.build, 'npm run build:govuk && npm run build:govuk-pages');
 assert.equal(
 	packageJson.scripts['build:govuk'],
 	'sass --load-path=node_modules --no-source-map --style=compressed src/styles/govuk.scss public/assets/govuk/govuk-frontend.css && node scripts/govuk/copy-govuk-assets.mjs',
 );
+assert.equal(packageJson.scripts['build:govuk-pages'], 'node scripts/govuk/render-govuk-pages.mjs');
 assert.match(packageLock, /"govuk-frontend": "\^6\.0\.0"/);
+assert.match(packageLock, /"nunjucks": "\^3\.2\.4"/);
 assert.match(packageLock, /"node_modules\/govuk-frontend"/);
 assert.match(packageLock, /"version": "6\.1\.0"/);
 
@@ -44,32 +47,41 @@ assert.match(sassEntry, /@import ['"]govuk-frontend\/dist\/govuk['"];/);
 assert.match(copyScript, /node_modules\/govuk-frontend\/dist\/govuk/);
 assert.match(copyScript, /public\/assets\/govuk/);
 assert.match(copyScript, /govuk-frontend\.min\.js/);
+assert.match(renderScript, /new nunjucks\.Environment/);
+assert.match(renderScript, /node_modules\/govuk-frontend\/dist/);
+assert.match(renderScript, /pages\/home\.njk/);
+assert.match(renderScript, /pages\/account\.njk/);
+assert.match(renderScript, /pages\/start-overview\.njk/);
 assert.match(initScript, /import \{ initAll \} from '\/assets\/govuk\/govuk-frontend\.min\.js';/);
 assert.match(initScript, /initAll\(\{ scope \}\);/);
 assert.match(initScript, /x-include:loaded/);
 
+assert.match(layoutTemplate, /govuk\/components\/header\/macro\.njk/);
+assert.match(layoutTemplate, /govukHeader\(\{/);
+assert.match(layoutTemplate, /productName: serviceName/);
+assert.match(layoutTemplate, /govuk\/components\/service-navigation\/macro\.njk/);
+assert.match(layoutTemplate, /govukServiceNavigation\(\{/);
+assert.match(layoutTemplate, /govuk\/components\/footer\/macro\.njk/);
+assert.match(homeTemplate, /govuk\/components\/button\/macro\.njk/);
+assert.match(homeTemplate, /govuk\/components\/tag\/macro\.njk/);
+
 for (const path of representativePages) {
 	const page = fs.readFileSync(path, 'utf8');
 	assert.match(page, /\/assets\/govuk\/govuk-frontend\.css/, `${path} should load generated GOV.UK Frontend CSS`);
+	assert.match(page, /<html class="govuk-template" lang="en">/, `${path} should use the GOV.UK template class`);
+	assert.match(page, /class="govuk-template__body"/, `${path} should use GOV.UK template body class`);
+	assert.match(page, /govuk-frontend-supported/, `${path} should include GOV.UK Frontend support snippet`);
+	assert.match(page, /govuk-header__product-name/, `${path} should render GOV.UK header product name from the macro`);
+	assert.match(page, /ResearchOps Demo Suite/, `${path} should render the ResearchOps service name`);
+	assert.match(page, /govuk-service-navigation/, `${path} should render GOV.UK service navigation`);
+	assert.match(page, /govuk-footer/, `${path} should render GOV.UK footer`);
 
 	for (const customAsset of customCssAssets) {
 		assert.equal(page.includes(customAsset), false, `${path} should not load custom GOV.UK clone CSS asset ${customAsset}`);
 	}
-}
 
-for (const path of representativeFullPages) {
-	const page = fs.readFileSync(path, 'utf8');
-	assert.match(page, /<html class="govuk-template" lang="en">/, `${path} should use the GOV.UK template class`);
-	assert.match(page, /class="govuk-template__body"/, `${path} should use GOV.UK template body class`);
-	assert.match(page, /govuk-frontend-supported/, `${path} should include GOV.UK Frontend support snippet`);
-	assert.match(page, /\/js\/govuk-frontend-init\.js/, `${path} should initialise GOV.UK Frontend JavaScript`);
+	assert.equal(page.includes('<x-include'), false, `${path} should not use custom x-include page chrome`);
 }
-
-assert.equal(headerPartial.includes('/css/govuk/govuk-header-service-brand.css'), false);
-assert.equal(headerPartial.includes('/css/govuk/govuk-main-content-focus.css'), false);
-assert.equal(headerPartial.includes('govuk-header__homepage-link'), false);
-assert.match(headerPartial, /govuk-header__content/);
-assert.match(headerPartial, /govuk-header__service-name/);
 
 for (const path of [
 	'public/assets/govuk/govuk-frontend.css',
