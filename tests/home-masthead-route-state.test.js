a280d0c648dd7c1cd0ee61af16cf0ac612e0eea5
Kevin Rapley <digikev.kevin.rapley@gmail.com>
2026-06-08 16:45:47 +0100

    Remove unlinked CSS assertion from home masthead test

--- tests/home-masthead-route-state.test.js
+++ tests/home-masthead-route-state.test.js
@@ -3,7 +3,6 @@ import fs from 'node:fs';
 
 const template = fs.readFileSync('src/govuk/templates/pages/home.njk', 'utf8');
 const publicHtml = fs.readFileSync('public/index.html', 'utf8');
-const mastheadCss = fs.readFileSync('public/assets/researchops/researchops-home-masthead.css', 'utf8');
 const svg = fs.readFileSync('public/images/home-masthead-researchops-illustration.svg', 'utf8');
 
 function has(source, text, label) {
@@ -19,17 +18,20 @@ lacks(template, 'home-breadcrumbs', 'home template');
 has(template, 'app-masthead researchops-home-masthead', 'home template');
 has(template, 'researchops-home-hero__image-column', 'home template');
 has(template, 'home-masthead-researchops-illustration.svg', 'home template');
+has(template, 'width="320" height="198"', 'home template');
 
 lacks(publicHtml, 'id="home-breadcrumbs"', 'rendered home page');
 has(publicHtml, 'app-masthead researchops-home-masthead', 'rendered home page');
 has(publicHtml, 'home-masthead-researchops-illustration.svg', 'rendered home page');
-
-has(mastheadCss, '.researchops-home-hero__image-column', 'masthead CSS');
-has(mastheadCss, '.researchops-home-hero__image', 'masthead CSS');
-has(mastheadCss, 'max-width: 320px;', 'masthead CSS');
+has(publicHtml, 'width="320"', 'rendered home page');
+has(publicHtml, 'height="198"', 'rendered home page');
 
 has(svg, '<title>Research operations illustration</title>', 'home SVG');
 has(svg, 'ResearchOps-home-illustration', 'home SVG');
 has(svg, 'Workflow-connectors', 'home SVG');
 has(svg, 'Study-plan-card', 'home SVG');
+has(svg, 'Participant-operations-card', 'home SVG');
+has(svg, 'Moderated-session-panel', 'home SVG');
+has(svg, 'Live-notes-card', 'home SVG');
 has(svg, 'ResearchOps-laptop', 'home SVG');
+has(svg, 'Evidence-trail-cards', 'home SVG');
