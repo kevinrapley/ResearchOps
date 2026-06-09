9322a287b5e8c19e9cb37cbccbda2779ef7469cc
Kevin Rapley <digikev.kevin.rapley@gmail.com>
2026-06-09 12:56:40 +0100

    Update brand route-state test for Home Office SVG header

--- tests/brand-variant-route-state.test.js
+++ tests/brand-variant-route-state.test.js
@@ -15,9 +15,9 @@ assert.ok(sharedHeader.includes('researchops-home-office-brand'));
 assert.ok(sharedHeader.includes('/css/brands/home-office.css'));
 assert.ok(sharedHeader.includes('/js/brand-variant.js'));
 assert.ok(sharedHeader.includes('researchops-header__home-office-logo'));
+assert.ok(sharedHeader.includes('hods-header__logo-svg'));
 assert.ok(sharedHeader.includes('viewBox="0 0 578 138"'));
 assert.ok(sharedHeader.includes('#732282'));
-assert.ok(sharedHeader.includes('govuk-header__logotype researchops-header__logotype'));
 
 assert.ok(brandScript.includes('home-office'));
 assert.ok(brandScript.includes('govuk'));
